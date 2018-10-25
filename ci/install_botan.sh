7ccb77afa0e797579cadbdbe19bcc49aa9ab34c0
Sebastian Skałacki <skalee@gmail.com>
2018-10-25 19:48:49 +0200

    Improve dependency building
    Replace a large script which install dependencies with smaller ones,
    copied from riboseinc/enmail repository.  Update .travis.yml to use
    these scripts.

---
+++ ci/install_botan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# (c) Copyright 2018 Ribose Inc.
+#
+
+# Based on:
+# https://github.com/riboseinc/ruby-rnp/blob/52d6113458cb095cf7811/ci/install.sh
+
+set -eux
+
+: "${CORES:=2}"
+: "${MAKE:=make}"
+
+botan_build="${DEPS_BUILD_DIR}/botan"
+
+if [ ! -e "${BOTAN_PREFIX}/lib/libbotan-2.so" ] && \
+	 [ ! -e "${BOTAN_PREFIX}/lib/libbotan-2.dylib" ]; then
+
+	if [ -d "${botan_build}" ]; then
+		rm -rf "${botan_build}"
+	fi
+
+	git clone --depth 1 https://github.com/randombit/botan "${botan_build}"
+	pushd "${botan_build}"
+	./configure.py --prefix="${BOTAN_PREFIX}" --with-debug-info --cxxflags="-fno-omit-frame-pointer"
+	${MAKE} -j${CORES} install
+	popd
+fi
