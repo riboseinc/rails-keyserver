a745d920430b4d1335b6b08bc74c550e481dadc1
Jeffrey Lau <jeffrey.lau@ribose.com>
2019-03-11 18:32:17 +0800

    spec: Fix encryption key used in tests to 32 bytes as required by 'encryptor'

--- spec/test_app/config/initializers/master_key.rb
+++ spec/test_app/config/initializers/master_key.rb
@@ -1,1 +1,2 @@
-Rails::Keyserver::Engine.config.encryption_key = "hi" * 32
+# Truncate key to 32 bytes as required by encryptor gem
+Rails::Keyserver::Engine.config.encryption_key = ("hi" * 32)[0...32]
